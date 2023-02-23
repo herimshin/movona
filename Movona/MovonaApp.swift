7983a1cc15f935b99b407d6b5df01bc77ad694eb
Hérim <herimshin@gmail.com>
2023-02-23 13:38:48 +0100

    Initial Commit

---
+++ Movona/MovonaApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovonaApp.swift
+//  Movona
+//
+//  Created by 신혜림 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MovonaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
